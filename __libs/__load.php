b8a6cc6da5801dfda69beca1344f23d94e5b956c
jdk-dharshankumar <dharshankumarlearn@gmail.com>
2024-01-01 22:12:00 +0530

    inserting values in databases can b done

---
+++ __libs/__load.php
@@ -0,0 +1,38 @@
+<? 
+
+function signup($user ,$email,$pass,$phone){
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "Sridharsh@12";
+  $dbname = "_php_conn";
+  
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $sql = "INSERT INTO `isgn` (`username`, `email`, `password`, `phonenumber`)
+  VALUES ('$user', '$email', '$pass', '$phone');";
+  $result= false;
+
+  if ($conn->query($sql) === TRUE) {
+    $result= true;
+  }else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    $result=false;
+  }
+
+  $conn->close();
+  return $result;
+
+}
+
+
+?>
+
+
+ 
+
